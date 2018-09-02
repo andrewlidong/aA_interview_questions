4b70c2c077a2f8b361dae58b068f3e0389f730dd
Andrew Dong <andrewdong1994@gmail.com>
2018-09-02 12:52:45 -0400

    added reverse

--- D1B/reverse.js
+++ D1B/reverse.js
@@ -9,3 +9,16 @@
 //
 // What is the time complexity of changing a character in a string?
 // Note: Ruby strings, unlike JavaScript, are mutable
+
+function reverse(str) {
+  const chars = str.split('');
+  const n = chars.length;
+
+  for (let i = 0; i < n / 2; i ++ ) {
+    let t = chars[i];
+    chars[i] = chars[n - i - 1];
+    chars[n - i - 1] = t;
+  }
+
+  return chars.join('');
+}
