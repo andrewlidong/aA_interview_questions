ba63d0e6f87d856a83133092307dec2d28503fed
Andrew Dong <andrewdong1994@gmail.com>
2018-09-16 14:01:01 -0400

    D15, D16

--- D15A/streaming_sample.rb
+++ D15A/streaming_sample.rb
@@ -6,6 +6,23 @@
 # Use only O(1) memory.
 # Every value in the stream should have an equal probability of being sampled.
 
+def streaming_sample(stream)
+  sample = stream
+  num_els = 1 # needs to set to the first stream because otherwise first one never gets picked
+
+  while true
+    next_value = stream.next_value
+    break if next_value.nil?
+
+    # keep sample with probability 1 / (num_els + 1)
+    keep_prob = 1.fdiv(num_els + 1)
+    sample = next_value if rand() < keep_prob
+
+    num_els += 1
+  end
+
+  sample
+end
 
 
 # Let's prove this works by induction. First, note that for num_els = 1, this says we keep the previous sample (nil), with probability 0. So after 1 element, every element has an equal chance of being sampled (the only element is selected with probability 1).
