38210a53746a6f9209adb8f46f92941cff5f25c3
Andrew Dong <andrewdong1994@gmail.com>
2018-09-16 13:50:52 -0400

    D13, D14

--- D13A/rand7.rb
+++ D13A/rand7.rb
@@ -1,2 +1,12 @@
 # rand7
 # Write a method to generate a random integer (0...7), given a method that generates a random integer between (0...5). The resulting rand7 distribution must be uniform.
+
+def rand7
+  while true
+    # Construct a random number (0...5*2)
+    num = 5 * (rand5) + rand5
+    return (num % 7) if num < 21
+
+    # reject 21, 22, 23, 24
+  end
+end
--- D13B/square_root.rb
+++ D13B/square_root.rb
@@ -2,3 +2,19 @@
 # Implement a square root function that uses only addition, subtraction, multiplication and division in less than linear time. You may assume that input is always a perfect square.
 #
 # Hint: One naive solution has a better time complexity than many people realize--iterating from 0 until the square root is going to be O(sqrt n), not O(n). However, we can do better!
+
+# You know it won't be as fast as constant time, but it could be logarithmic.
+# What's our favorite logarithmic algorithm? Binary search!
+def sqroot(num, candidates = nil)
+  candidates ||= (0..num / 2).to_a
+  middle = candidates.length / 2
+  case num <=> (candidates[middle] * candidates[middle])
+  when -1
+    sqroot(num, candidates.take(middle))
+  when 0
+    middle
+  when 1
+    sub_answer = sqroot(num, candidates.drop(middle + 1))
+    (sub_answer.nil) ? nil : (middle + 1) + sub_answer
+  end
+end
