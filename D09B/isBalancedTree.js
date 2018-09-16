0eff39a2adbe7c6433c8db7cdd65cdb3b0b986a3
Andrew Dong <andrewdong1994@gmail.com>
2018-09-16 12:32:53 -0400

    D09

--- D09B/isBalancedTree.js
+++ D09B/isBalancedTree.js
@@ -1,2 +1,75 @@
 // isBalancedTree
 // Write a JavaScript function to check if a binary tree is balanced. A tree is balanced if, at every node, the depth of subtree on the left hand side is equal to the depth of the subtree on the right (plus or minus one).
+
+// Take in the root node
+function isBalanced(node) {
+  // Base case: the tree is empty.  Return true.
+
+  if (!node) {
+    return true;
+  }
+
+  // Get the depths of left and right subtrees and compare
+  var leftDepth = getDepth(node.left);
+  var rightDepth = getDepth(node.right);
+  var depthDiff = Math.abs(leftDepth - rightDepth);
+
+  // The tree is balanced if both subtrees are balanced and the difference in depths of those subtrees is betseen -1 and 1.
+  return isBalanced(node.left) && isBalanced(node.right) && depthDiff < 2;
+}
+
+function getDepth(node) {
+  // Base case: empty tree.  Depth is 0.
+  if (!node) {
+    return 0;
+  }
+
+  // Take the larger depth of the two subtrees, calculated recursively
+  return Math.max(getDepth(node.left), getDepth(node.right)) + 1;
+}
+
+// This solution calculates the depth in a separate recursive call for a time complexity of O(n^2). We can optimize by tracking the depth in the same recursive call for a time complexity of O(n).
+
+function optimizedIsBalanced(node, depthCount) {
+  if (!node) {
+    depthCount.depth = 0;
+    return true;
+  }
+
+  const leftDepthCount = { depth: 0 };
+  const rightDepthCount = { depth: 0 };
+  const isLeftBalanced = optimizedIsBalanaced(node.left, leftDepthCount);
+  const isRightBalanced = optimizedIsBalanaced(node.right, rightDepthCount);
+  const leftDepth = leftDepthCount.depth;
+  const rightDepth = rightDepthCount.depth;
+
+  depthCount.depth = Math.max(leftDepth, rightDepth) + 1;
+
+  if (Math.abs(leftDepth - rightDepth) >= 2) {
+    return false;
+  } else {
+    return isLeftBalanced && isRightBalanced;
+  }
+}
+
+function _optimizedIsBalanced(node) {
+  if (!node) return 0;
+
+  const leftDepth = optimizedIsBalanced(node.left);
+  // left side was unbalanced: return -1 up the callstack
+  if (leftDepth < 0) return leftDepth;
+  const rightDepth = optimizedIsBalanced(node.right);
+  // right side was unbalanced: return -1 up the callstack
+  if (rightDepth < 0) return rightDepth;
+
+  if (Math.abs(leftDepth - rightDepth) >= 2) {
+    // The tree is unbalanced
+    return -1;
+  } else {
+    return Math.max(leftDepth, rightDepth) + 1;
+  }
+}
+
+function optimizedIsBalanced(node) {
+  return _optimizedIsBalanced(node) !== -1;
+}
--- D09B/next.rb
+++ D09B/next.rb
@@ -2,3 +2,33 @@
 # Given a node in a Binary Search Tree, find the next node in the tree. In this case, we can think of ordering being an in-order traversal of the tree where next is smallest node value which is greater or equal to the given node.
 #
 # Assume you don't have the root of the tree, just a single node from it.
+
+# O(log(n)) if BST is balanced
+def next(node)
+  if node.right
+    # find smallest node to the right
+    return left_most_node(node.right)
+  end
+
+  # no nodes to the right; climb up
+  until true
+    parent_node = node.parent
+    if parent_node.nil?
+      # at the top of the tree, and nothing bigger to the rght
+      return nil
+    elsif parent_node.left == node
+      # parent is bigger than us
+      return parent_node
+    else
+      # parent is smaller, keep climbing
+      node = parent_node
+    end
+  end
+end
+
+def left_most_node(node)
+  # keep going down and to the left
+  node = node.left until node.left.nil?
+
+  node
+end
